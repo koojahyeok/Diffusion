8dd0025c9fe7a0bba3cd69c17de24ad93549e40d
koojahyeok <gjh7106@snu.ac.kr>
2023-11-17 15:24:38 +0900

    stable diffusion

---
+++ stable_diffusion/model/__init__.py
@@ -0,0 +1,12 @@
+"""
+---
+title: Modules used in stable diffusion
+summary: >
+ Models and components for stable diffusion.
+---
+
+stable diffusion model
+논문 제목 : High-Resolution Image Synthesis with Latent Diffusion Models
+made by jahyeok
+
+"""
